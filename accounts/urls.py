07d337ddd28f5d98a1aef07ae4d4d49df7d98bdd
SimonOkello <simonokello93@gmail.com>
2020-07-23 14:58:55 +0300

    Finished username validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 
-from accounts.views import LoginView, Register
+from accounts.views import LoginView, Register, UsernameValidationView
 
 urlpatterns = [
 
-    # path('login/', LoginView.as_view(), name = 'login'),
+    path('validate_username',csrf_exempt(UsernameValidationView.as_view()), name = 'username-validate'),
     path('register/', Register.as_view(), name = 'register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
 # Create your views here.
+
+
+class UsernameValidationView(View):
+
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should not contain alphanueric.'}, status=400)
+        return JsonResponse({'username_valid': True})
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is taken'}, status=409)
+        return JsonResponse({'username_valid':True})
 
 
 class LoginView(View):
