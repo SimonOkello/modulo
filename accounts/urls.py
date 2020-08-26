f33219be570b7efa3b0bda8e4c9db6764da6fe9c
SIMON DENNIS <simonokello93@gmail.com>
2020-08-26 22:10:33 +0300

    Finished password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name = 'login'),
     path('logout', userLogout, name = 'logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name = 'password_reset'),
-    path('reset_email_sent/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
-    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
+    path('reset_email_sent/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name = 'password_reset_confirm'),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name = 'password_reset_complete'),
 ]
