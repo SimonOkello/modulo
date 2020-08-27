0d70de3c1e095a62c53fa5400f6bba23e3a3b778
SIMON DENNIS <simonokello93@gmail.com>
2020-08-27 16:03:44 +0300

    Fix failure to create user settings during registration

--- usersettings/models.py
+++ usersettings/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class userSetting(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    currency = models.CharField(max_length=255, blank=True, null=True)
+    currency = models.CharField(max_length=255, default='KES - Kenyan Shilling')
 
     def __str__(self):
         return str(self.user)+ ' '+ 'Settings'
+
+def create_user_settings(sender, instance, created, **kwargs):
+    
+    if created:
+
+        userSetting.objects.create(user=instance)
+
+post_save.connect(create_user_settings, sender=User)
